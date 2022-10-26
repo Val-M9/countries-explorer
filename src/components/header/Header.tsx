8616eac4ee333ef4c0e551f70b3fa5444a36e47f
Val_M <v.mischenko90@gmail.com>
2022-10-26 21:07:06 +0300

    add base styles working on header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import '../../styles/base.scss';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <div className='header content'>
+      <h1 className='heading'>Where in the world</h1>
+
+    </div>
+  );
+};
+
+export { Header };
